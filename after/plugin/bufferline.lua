e64e2820108add2f33c4d5968b56e5bab7230cc0
Gabriel Albuquerque <albuquerque.se@proton.me>
2023-08-02 18:20:08 -0300

    fix: to keep keymaps when working with buffer

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,3 +1,6 @@
 -- bufferline.lua
 vim.opt.termguicolors = true
 require("bufferline").setup{}
+
+vim.api.nvim_set_keymap('n', 'gt', '<cmd>BufferLineCycleNext<CR>', {})
+vim.api.nvim_set_keymap('n', 'gT', '<cmd>BufferLineCyclePrev<CR>', {})
