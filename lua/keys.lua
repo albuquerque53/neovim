0b4a19776e331328d22749cf2b9284822926f00a
Gabriel Albuquerque <albuquerque.se@proton.me>
2023-08-08 14:57:33 -0300

    feat: binds to multiselect

--- lua/keys.lua
+++ lua/keys.lua
@@ -1,5 +1,5 @@
 -- keys.lua
--- all the keybinds are here
+-- general and non plugin specific keybinds are here
 vim.g.mapleader = ' '
 vim.api.nvim_set_keymap('n', '<leader>cl', ':bd<cr>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>ca', ':bufdo bd<cr>', { noremap = true })
@@ -8,4 +8,6 @@ vim.api.nvim_set_keymap('n', '<leader>fce', ':edit ~/.config/nvim/init.lua<cr>',
 vim.api.nvim_set_keymap('n', '<leader>fpe', ':edit ~/.config/nvim/lua/plugins.lua<cr>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>fps', ':vsplit ~/.config/nvim/lua/plugins.lua<cr>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>fcl', ':luafile ~/.config/nvim/init.lua<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>ho', ':nohlsearch<cr>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>pi', ':PackerInstall<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-n>', '*N', { noremap = true })
