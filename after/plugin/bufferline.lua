209f49c2af1e49e8cc0fd4788ab3862eab73e0f9
Gabriel Albuquerque <albuquerque.se@proton.me>
2023-08-17 21:27:48 -0300

    fix: bufferline config

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,6 +1,13 @@
 -- bufferline.lua
 vim.opt.termguicolors = true
-require("bufferline").setup{}
+
+require("bufferline").setup{
+    options = {
+        always_show_bufferline = false,
+        show_close_icon = false,
+        color_icons = true
+    }
+}
 
 vim.api.nvim_set_keymap('n', 'gt', '<cmd>BufferLineCycleNext<CR>', {})
 vim.api.nvim_set_keymap('n', 'gT', '<cmd>BufferLineCyclePrev<CR>', {})
